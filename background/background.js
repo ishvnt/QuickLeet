3b3b89725f9edbf9446f342bae6a444118df8943
ishvnt <ishantdahiya@outlook.com>
2024-12-18 20:42:42 +0530

    moved stuff to createFile() function

--- background/background.js
+++ background/background.js
@@ -2,21 +2,27 @@ async function getTitle() {
     let tabs = await browser.tabs.query({ active: true, currentWindow: true });
     return tabs[0].url;
 }
-async function downloadFile() {
+async function createFile()
+{
     let text = await getTitle();
     text = text.split("/")[4];
     const blob = new Blob([text], { type: 'text/plain' });
     const fileurl = URL.createObjectURL(blob);
-    const filename = `${text}.cpp`
+    const filename = `${text}.cpp`;
 
-    const downloading = browser.downloads.download({
+    const file = {
         url: fileurl,
         filename: filename,
         saveAs: true,
         conflictAction: "uniquify"
-    })
-
-    downloading.then((id) => { console.log(id) }, (error) => { console.log(error) })
+    };
+    
+    return file;
+}
+async function downloadFile() {
+    const file = await createFile();
+    const downloading = browser.downloads.download(file);
+    downloading.then((id) => { console.log(id) }, (error) => { console.log(error) });
 }
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "generate") {
